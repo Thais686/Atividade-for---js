15184f1aa25c1bbbc0f65576167bbf29f8138dd0
Thaís <thaiscastro95@yahoo.com>
2024-09-24 15:52:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let numero
+let i=0
+let soma=1
+numero= prompt("Digite quantos elefantes")
+
+
+for(i=1; i<=numero ;i++){
+
+    console.log( `${i} elefantes incomoda muita gente` )
+
+    soma=i+1
+    console.log(`${soma} elefantes incomodam muito mais`)
+
+}
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,17 @@
+
+let i=0
+let soma=0
+ let multiplicacao=0
+
+
+for(i=0; i<20; i++){
+
+
+soma= soma *2 +1
+console.log(soma)
+
+multiplicacao=multiplicacao + soma
+}
+
+
+console.log(`Resultado da soma final ${multiplicacao}`)
